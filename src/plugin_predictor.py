f6e6d6c8c3d6273524205def2f6519f22541d1ba
Arpan Agrawal <arpan2@illinois.edu>
2023-02-21 15:50:11 -0600

    Command line arguments for tactic prediction instead of interactive.

--- src/plugin_predictor.py
+++ src/plugin_predictor.py
@@ -20,36 +20,34 @@ def predict(args: List[str]) -> None:
     parser.add_argument("weightsfile", default=None, type=Path)
     parser.add_argument("-k", "--num-predictions", default=5)
     parser.add_argument("--print-certainties", action='store_true')
-    parser.add_argument("--rel_lemmas", default=[], type=List[str])
-    parser.add_argument("--prev_tactics", default=[], type=List[str])
-    parser.add_argument("--hypotheses", default=[], type=List[str])
+    parser.add_argument("--rel_lemmas", nargs="*", default=[], type=str)
+    parser.add_argument("--prev_tactics", nargs="*", default=[], type=str)
+    parser.add_argument("--hypotheses", nargs="*", default=[], type=str)
     parser.add_argument("--goal", default="", type=str)
     arg_values = parser.parse_args(args)
 
+    # print(arg_values)
+
     predictor = get_predictor(parser, arg_values)
+    rel_lemmas = arg_values.rel_lemmas
+    prev_tactics = arg_values.prev_tactics
+    hypotheses = arg_values.hypotheses
+    goal = arg_values.goal
 
-    while True:
-        
-        
-
-        if goal.strip() == "":
-                print("Exiting...")
-                break
-        
-        tac_context = TacticContext(rel_lemmas,
+    tac_context = TacticContext(rel_lemmas,
                                     prev_tactics,
                                     hypotheses,
                                     goal)
 
-        predictions = predictor.predictKTactics(
+    predictions = predictor.predictKTactics(
             tac_context, arg_values.num_predictions)
 
-        for prediction in predictions:
-            if arg_values.print_certainties:
-                print(f"Prediction: \"{prediction.prediction}\"; "
-                      f"certainty: {prediction.certainty}")
-            else:
-                print(prediction.prediction)
+    for prediction in predictions:
+        if arg_values.print_certainties:
+            print(f"Prediction: \"{prediction.prediction}\"; "
+                    f"certainty: {prediction.certainty}")
+        else:
+            print(prediction.prediction)
 
 if __name__ == "__main__":
-    pass
+    predict(sys.argv[1:])
