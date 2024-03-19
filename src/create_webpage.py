68a904e0468e77da67d8a5042b345a4c52772c7e
Arpan Agrawal <arpan2@illinois.edu>
2024-03-19 14:18:56 -0500

    Fixing browser bug for macOS.

--- src/create_webpage.py
+++ src/create_webpage.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import os
 import webbrowser
 
 def main():
@@ -15,32 +16,25 @@ def main():
 
     with open("proved_theorem.html") as fp:
         soup = BeautifulSoup(fp, "lxml")
-        # for link in soup.findAll('link'):
-        #     link['href'] = link['href'].replace("alectryon.css", "{{url_for('static', filename='alectryon.css')}}")
-        #     link['href'] = link['href'].replace("pygments.css", "{{url_for('static', filename='pygments.css')}}")
-        # for script in soup.findAll('script'):
-        #     script['src'] = script['src'].replace("alectryon.js", "{{url_for('static', filename='alectryon.js')}}")
         for div in soup.find_all("div", {'class':'alectryon-banner'}): 
             div.decompose()
         if (soup.title is not None):
             new_title = soup.new_tag("title")
             new_title.string = "Synthesis Results"
             soup.title.replace_with(new_title)
-        # back_button = soup.new_tag("a", href="/", **{"class":"button"})
-        # back_button.string = "Go back"
-        # soup.head.append(back_button)
         soup.head.append(soup.new_tag("script", src="d3.min.js"))
         soup.head.append(soup.new_tag("link", rel="stylesheet", href="d3-min.css"))
-        # soup.head.append(soup.new_tag("link", rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"))
-        # soup.head.append(soup.new_tag("link", rel="stylesheet", href="{{url_for('static', filename='footer.css')}}"))
         soup.body.append(soup.new_tag("script", src="d3-tree.js"))
-        # soup.body.insert_before("{% include 'title.html' %}")
-        # soup.body.append("{% include 'footer2.html' %}")
         with open("modified_html.html", "w") as fp2:
             fp2.write(soup.prettify())
         fp2.close()
     fp.close()
-    webbrowser.open("modified_html.html", new=2)
+    if (os.name == 'posix'):
+        filepath = os.getcwd()
+        fileuri = 'file:///' + filepath + '/modified_html.html'
+        webbrowser.open_new_tab(fileuri)
+    else:
+        webbrowser.open("modified_html.html", new=2)
 
 if __name__ == "__main__":
     main()
