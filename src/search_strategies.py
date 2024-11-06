8c950e697331d0a0d7bf63f6c6bfb13ad80d7e6e
Stuart Pernsteiner <spernsteiner@galois.com>
2024-11-05 17:08:04 -0800

    search_strategies: write json graph into output_dir

--- src/search_strategies.py
+++ src/search_strategies.py
@@ -547,7 +547,7 @@ def dfs_proof_search_with_graph(lemma_name: str,
         pbar.clear()
     if args.generate_graph:
         g.draw(f"{output_dir}/{module_prefix}{lemma_name}.svg")
-    with open(f"{module_prefix}-json_graph.txt", "w") as graph_json:
+    with open(f"{output_dir}/{module_prefix}{lemma_name}.graph.json", "w") as graph_json:
         json.dump(g.print_label_recursive(g.start_node), graph_json)
     if args.features_json:
         g.write_feat_json(f"{output_dir}/{module_prefix}"
